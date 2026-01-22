c87106e15fdf1fd2606694a7a3af204c06924de1
Ratan <ratan.ajit.singh.2@gmail.com>
2026-01-22 14:24:11 +0100

    Added sidebar to be able to change the configurations in the app itself rather than having to change it in the code

--- src/app.py
+++ src/app.py
@@ -16,15 +16,15 @@ API_KEY = os.getenv("NEWS_API_KEY")
 
 
 #fetches the top (size) news articles about a topic of choice
-def fetch_news(topic):
+def fetch_news(topic, days, limit):
 
     #creating a buffer for incomplete data
-    buffer = 100
+    buffer = limit
     #define the size required
-    size = 100
+
 
     #this makes sure that it takes in from the last 7 days
-    start_date = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
+    start_date = (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d")
     if not API_KEY:
         return []
 
@@ -46,7 +46,7 @@ def fetch_news(topic):
             if art.get("title") and art["title"] != "[Removed]":
                 valid_articles.append(art)
 
-        return valid_articles[:size]
+        return valid_articles[:limit]
     except Exception as error:
         print(f"Failed : {error}")
         return []
@@ -84,15 +84,44 @@ def analyze_sentiment(articles):
 def main():
     st.set_page_config(page_title="Sentiment Analysis", page_icon="📑")
 
+    #settings config
+    st.sidebar.header("Analysis Settings")
+
+    days_choice = st.sidebar.slider(
+        "Days to look back",
+        min_value =1,
+        max_value =28,
+        value = 7,
+        help = "How far back do you want to search for news?"
+    )
+
+    limit_choice = st.sidebar.slider(
+        "Maximum number of articles to analyze",
+        min_value = 10,
+        max_value = 100,
+        value = 50,
+        step =10,
+        help = "How many articles do you want to analyze? (more articles means a longer processing time)."
+    )
+
+    st.sidebar.markdown("---")
+    st.sidebar.info("Adjust the settings to the analysis to refine search results.")
+
+    #main page
+
     st.title("News Sentiment Analysis")
     st.write("Enter a topic of interest to see the latest sentiment trends.")
 
-    #search
-    topic = st.text_input("Search", "Artificial Intelligence")
+    col1, col2 = st.columns([3,1])
+    with col1:
+        topic = st.text_input("Search", "Artificial Intelligence")
+    with col2:
+        st.write("")
+        st.write("")
 
     if st.button("Analyze Sentiment"):
         with st.spinner("Analyzing Sentiment..."):
-            raw_articles = fetch_news(topic)
+            raw_articles = fetch_news(topic, days_choice, limit_choice)
 
         #main statistic
         if raw_articles:
@@ -106,13 +135,15 @@ def main():
             avg_sent = df['sentiment'].mean()
 
 
-            col1, col2 = st.columns(2)
-            with col1:
+            m_col1, m_col2, m_col3 = st.columns(3)
+            with m_col1:
                 st.metric("Amount of articles analyzed", len(result))
-            with col2:
+            with m_col2:
                 sentiment_color = "normal"
                 if avg_sent > 0.1: sentiment_color = "off"
                 st.metric("Average Sentiment", f"{avg_sent:.2f}")
+            with m_col3:
+                st.metric("TimeFrame", f"Last {days_choice} days")
 
             st.markdown("---")
 
@@ -140,7 +171,8 @@ def main():
 
             #bar graph
             st.subheader("Sentiment Distribution")
-            top_df = df.head(20)
+            top_n = min(20, limit_choice)
+            top_df = df.head(top_n)
             #the bar chart showing individual sentiments
             figure = px.bar(top_df, x = 'title', y = 'sentiment',
                             color = "sentiment",
